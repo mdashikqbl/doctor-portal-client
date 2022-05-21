c9ab7fe512bc96528cd49611ca5d2970ff8962cc
mdashikqbl <mdashikqbl@gmail.com>
2022-05-21 10:28:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navber from './Pages/Shared/Navber';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Navber></Navber>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+
+
     </div>
   );
 }
---
+++ src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h3>into your about detail</h3>
+        </div>
+    );
+};
+
+export default About;
